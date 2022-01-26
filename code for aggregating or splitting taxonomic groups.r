99a9eb4c8250fa75e5509ff1c3570e48195ccea9
Colleen Cosgrove <38922310+ccosgro1@users.noreply.github.com>
2022-01-26 14:02:46 -0500

    Added in functional feeding group stats to the invertcommcode. Need to do any instances of forward selection still - did not have the mental capacity to do it at this moment - that will be the plan tomorrow.

--- code for aggregating or splitting taxonomic groups.r
+++ code for aggregating or splitting taxonomic groups.r
@@ -2,7 +2,8 @@
 ### Import OTU Taxonomy table
 # The rows in this table must be a perfect match to the columns in the sitesXspecies matrix
 # you will need to change the path for your computer, or import the data the way you like to
-otu.tax=read.csv("invert_taxonomy_file.csv")
+#otu.tax=read.csv("invert_taxonomy_file.csv")
+otu.tax=read.csv("C:/Users/crc31/Dropbox/Jennings Invertebrates/flyingFFG.csv")
 p=nrow(otu.tax)
 dim(otu.tax)
 names(otu.tax)
@@ -12,8 +13,8 @@ names(otu.tax)
 
 
 #Remove all non-dip taxa
-otu.tax=otu.tax[otu.tax$order=="Diptera",]
-commdata=dip
+otu.tax=otu.tax[otu.tax$FFG!="",]
+commdata=
 
 ###########################
 ### AGGREGATE OTUs to higher level taxonomic groups 
@@ -25,8 +26,8 @@ commdata=dip
 # This example creates a dataset of abundances for each class, and throws out sequences not validly assigned to a class (because including them would be meaningless)
 # Note that columns 1-10 in this dataset are the metadata, and there is some hard-coding column references around that 
 level=4
-levels(otu.tax[,level])
-types=levels(otu.tax[,level])
+levels(as.factor(otu.tax[,level]))
+types=levels(as.factor(otu.tax[,level]))
 q=length(types)
 n=length(commdata[,1])
 data.agg=matrix(nrow=n,ncol=q,0)
@@ -57,23 +58,25 @@ write.csv(data.agg,"invertcomm_genus.csv")
 
 # Let's select phylum Nematoda
 # So this example creates a dataset of Nematoda-only OTUs
-level=2
-levels(otu.tax[,level])
+level=5
+i=7
+levels(as.factor(otu.tax[,level]))
 # Choose your group in the next line. Make sure spelling is perfect.  Don't choose "FALSE" because that is meaningless.
-group="Diptera"
-data.nematoda=commdata[,otu.tax[,level]==group]
+spp.only=commdata[,8:158]
+group="woodfeeding"
+data.nematoda=spp.only[,otu.tax[,level]==group]
 dim(data.nematoda)
 
-data.nematoda=data.nematoda[c(1:18,22:38),]
+#data.nematoda=data.nematoda[c(1:18,22:38),]
 data.agg=data.nematoda
 data.agg=cbind(commdata[,1:7],data.agg)
 data.agg
 
 
-date4=data.agg[data.agg$Date=="10.15.16",]
-date2=data.agg[data.agg$Date=="5.5.16",]
-date3=data.agg[data.agg$Date=="8.10.16",]
-date1=data.agg[data.agg$Date=="10.6.15",]
+date4.agg=data.agg[data.agg$Date=="10.15.16",]
+date2.agg=data.agg[data.agg$Date=="5.5.16",]
+date3.agg=data.agg[data.agg$Date=="8.10.16",]
+date1.agg=data.agg[data.agg$Date=="10.6.15",]
 colsum1=colSums(date1[8:97])
 colsum2=colSums(date2[8:97])
 colsum3=colSums(date3[8:97])
